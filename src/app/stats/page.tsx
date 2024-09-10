3e5fd0636e2278b915202264e990e919f81c4011
Sean Ross-Ross <srossross@gmail.com>
2024-09-10 13:02:29 -0700

    dynamic

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -4,6 +4,9 @@ import LicenseDistribution from "./LicenseDistribution";
 import RiskDistribution from "./RiskDistribution";
 import HealthRiskNotes from "./HealthRiskNotes";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function StatsPage() {
   return (
     <div className="container mx-auto px-4 py-8">
