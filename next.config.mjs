3ba2bacd57ce1070a6348ed5ffec7d34b21fff69
Sean Ross-Ross <srossross@gmail.com>
2024-09-10 19:00:06 -0700

    try this config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   serverExternalPackages: ["duckdb"],
   experimental: {
     outputFileTracingIncludes: {
-      "/*+": "public",
+      "/**": ["./public/**"],
     },
   },
 };
