16faefa760cd0bc9b86ad0dfd6b39fcfa3347f07
Sean Ross-Ross <srossross@gmail.com>
2024-09-10 20:26:46 -0700

    improve search

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,12 @@
+import search_packages from "@/utils/search_packages";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  if (query == null || query.length == 0) {
+    return Response.json({ pkgs: [] });
+  }
+  const pgks = await search_packages(query);
+  return Response.json({ pgks });
+}
