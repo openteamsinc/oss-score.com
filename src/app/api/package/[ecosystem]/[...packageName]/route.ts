9fde7958ece9345d55ee44b5ae8799cd1fbc7001
Sean Ross-Ross <srossross@gmail.com>
2024-09-20 11:02:45 -0700

    Adding docs

--- src/app/api/package/[ecosystem]/[...packageName]/route.ts
+++ src/app/api/package/[ecosystem]/[...packageName]/route.ts
@@ -1,3 +1,4 @@
+import { cachedNotes, Notes } from "@/utils/database";
 import packageScore from "@/utils/packageScore";
 import { NextRequest } from "next/server";
 
@@ -7,15 +8,40 @@ type Props = {
     packageName: string[];
   };
 };
+
+type T = {
+  health_risk?: { notes?: string[] };
+  maturity?: { notes?: string[] };
+};
+function replaceNoteIdWithTextCode(val: T, notes: Notes) {
+  if (val.health_risk?.notes != null) {
+    val.health_risk.notes = val.health_risk.notes.map(
+      (noteId) => notes[noteId].code || "Unknown",
+    );
+  }
+  if (val.maturity?.notes != null) {
+    val.maturity.notes = val.maturity.notes.map(
+      (noteId) => notes[noteId].code || "Unknown",
+    );
+  }
+}
+
 export async function GET(
   request: NextRequest,
   { params: { ecosystem, packageName } }: Props,
 ) {
   const name = packageName.join("/");
   const { pkg, source } = await packageScore(ecosystem, name);
-  if (pkg == null) {
+  const notes = await cachedNotes();
+
+  if (pkg == null || source == null) {
     return Response.json({ error: "Not Found" }, { status: 404 });
   }
+  // @ts-expect-error dont care about packages for json response
+  delete source.packages;
+
+  replaceNoteIdWithTextCode(pkg, notes);
+  replaceNoteIdWithTextCode(source, notes);
 
   return Response.json({ ecosystem, name, source, pkg });
 }
