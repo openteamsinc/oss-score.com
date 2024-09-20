0d53ddd1f7c31c8a5b71dae8efe61ea6357f4942
Sean Ross-Ross <srossross@gmail.com>
2024-09-20 13:36:34 -0700

    Adding docs (#25)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+import createMDX from "@next/mdx";
 
 const nextConfig = {
   serverExternalPackages: ["duckdb"],
+  pageExtensions: ["mdx", "tsx", "ts"],
   experimental: {
     outputFileTracingIncludes: {
       "/**": ["./public/**"],
@@ -9,4 +11,9 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
