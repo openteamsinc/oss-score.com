6d5cbc84236b3417847259c26506b87468b44265
karamba228 <igoorderke@gmail.com>
2024-10-01 21:44:58 -0500

    Revert "combine ILIKE and damerau_levenshtein"
    This reverts commit 26bebf5db31bc2f96f160d1c64b77f898c01b17c.

--- src/utils/search_packages.ts
+++ src/utils/search_packages.ts
@@ -12,29 +12,17 @@ export type PackageResult = {
 
 export default async function search_packages(query: string) {
   const sqlQuery = `
-  WITH filtered_packages AS (
-    SELECT
-      packages.ecosystem,
-      packages.name,
-      scores.health_risk.value AS health_risk,
-      scores.maturity.value AS maturity,
-      packages.source_url
-    FROM packages
-    LEFT JOIN scores ON packages.source_url = scores.source_url
-    WHERE lower(packages.name) ILIKE lower(?::VARCHAR) || '%'  -- Prefix matching
-    LIMIT 100  -- Limit rows for Damerau-Levenshtein
-  )
   SELECT DISTINCT
-    ecosystem,
-    name,
-    health_risk,
-    maturity,
-    damerau_levenshtein(lower(name), lower(?::VARCHAR))::int AS name_distance
-  FROM filtered_packages
+    packages.ecosystem,
+    packages.name,
+    scores.health_risk.value AS health_risk,
+    scores.maturity.value AS maturity
+  FROM packages
+  LEFT JOIN scores ON packages.source_url = scores.source_url
+  WHERE lower(packages.name) ILIKE lower(?::VARCHAR) || '%'  -- Prefix matching
   ORDER BY
-    name_distance,
-    name
-  LIMIT 10;`;
+    packages.name
+  LIMIT 10`;
 
   try {
     const results = await fetchAll<PackageResult>(sqlQuery, query);
