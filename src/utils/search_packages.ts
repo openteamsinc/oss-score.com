6ad6079b4aa46779f60bafbdab6e0740a5a30b87
karamba228 <igoorderke@gmail.com>
2024-10-02 00:41:17 -0500

    testing

--- src/utils/search_packages.ts
+++ src/utils/search_packages.ts
@@ -12,17 +12,16 @@ export type PackageResult = {
 
 export default async function search_packages(query: string) {
   const sqlQuery = `
-   SELECT DISTINCT
+  SELECT
     packages.ecosystem,
     packages.name,
     scores.health_risk.value AS health_risk,
     scores.maturity.value AS maturity,
-    damerau_levenshtein(lower(name), lower(?::VARCHAR))::int AS name_distance
+    damerau_levenshtein(name, lower(?::VARCHAR))::int AS name_distance
   FROM packages
   LEFT JOIN scores ON packages.source_url = scores.source_url
   ORDER BY
       name_distance,
-      name
   LIMIT 10`;
 
   try {
