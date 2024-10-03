3c784c7c9fb17445fda86ce23d02e7a0233fdb29
karamba228 <igoorderke@gmail.com>
2024-10-02 21:17:15 -0500

    Revert "filter packages before join"
    This reverts commit 835dcb3944976d9e2443823008e3e50e11b06ff4.

--- src/utils/search_packages.ts
+++ src/utils/search_packages.ts
@@ -12,22 +12,17 @@ export type PackageResult = {
 
 export default async function search_packages(query: string) {
   const sqlQuery = `
-  WITH limited_packages AS (
   SELECT
     packages.ecosystem,
     packages.name,
+    scores.health_risk.value AS health_risk,
+    scores.maturity.value AS maturity,
     damerau_levenshtein(name, lower(?::VARCHAR))::int AS name_distance
   FROM packages
-  ORDER BY name_distance
-  LIMIT 10
-  )
-  SELECT
-    limited_packages.ecosystem,
-    limited_packages.name,
-    scores.health_risk.value AS health_risk,
-    scores.maturity.value AS maturity
-  FROM limited_packages
-  LEFT JOIN scores ON limited_packages.name = scores.source_url;`;
+  LEFT JOIN scores ON packages.source_url = scores.source_url
+  ORDER BY
+      name_distance
+  LIMIT 10`;
 
   try {
     const results = await fetchAll<PackageResult>(sqlQuery, query);
