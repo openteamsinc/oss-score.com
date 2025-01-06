ad562ef154c1aa9a42bcfa1eafe2725c3b8985de
Sean Ross-Ross <srossross@gmail.com>
2025-01-06 13:16:04 -0800

    update to use fetch (#50)

--- src/app/[ecosystem]/[...packageName]/page.tsx
+++ src/app/[ecosystem]/[...packageName]/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-import PackageScore from "./Score";
-import LoadingScore from "./LoadingScore";
+import { mdiAlert, mdiCircleSmall } from "@mdi/js";
+import Icon from "@mdi/react";
+import { notFound } from "next/navigation";
+
+import Risk from "@/components/Risk";
+import Maturity from "@/components/Maturity";
+import OtherStats from "@/components/OtherStats";
+import RiskHelp from "@/components/Help/Risk";
+import PackageStats from "@/components/Stats/PackageStats";
+
+import { fetchPackageScore, fetchNotes } from "@/utils/score_res";
 
 type Props = {
   params: Promise<{
@@ -10,17 +19,73 @@ type Props = {
   }>;
 };
 
-export default async function Package({ params }: Props) {
+export default async function PackageScoreComponent({ params }: Props) {
+  const notes = await fetchNotes();
   const { ecosystem, packageName } = await params;
   const name = packageName.join("/");
+  const {
+    package: pkg,
+    status,
+    score,
+  } = await fetchPackageScore(ecosystem, name);
+
+  if (status === "not_found") {
+    notFound();
+  }
+
   return (
-    <article className="container m-auto">
-      <h1 className="my-10 text-2xl font-bold">
-        {ecosystem}/{name}
-      </h1>
-      <React.Suspense fallback={<LoadingScore />}>
-        <PackageScore ecosystem={ecosystem} name={name} />
-      </React.Suspense>
-    </article>
+    <div className="grid grid-cols-2 gap-4">
+      <div>
+        <section className="mb-2">
+          <h2 className="m-2 flex items-center border-b border-b-slate-300 text-lg">
+            Health & Risk: <Risk value={score.health_risk.value} />
+          </h2>
+          <ul className="w-full list-inside space-y-2 text-sm text-slate-500">
+            {score.health_risk.notes
+              .filter((n) => n != null)
+              .map((noteId, index) => (
+                <li key={index} className="mb-2 flex items-start">
+                  <span className="h-5 px-2">
+                    <Icon
+                      path={mdiAlert}
+                      size={0.5}
+                      className="text-yellow-600"
+                    />
+                  </span>
+                  {notes[noteId]?.note || `Unknown id ${noteId}`}
+                  <RiskHelp
+                    note={notes[noteId]}
+                    ecosystem={ecosystem}
+                    packageName={name}
+                    score={score}
+                  />
+                </li>
+              ))}
+          </ul>
+        </section>
+        <section className="mb-2">
+          <h2 className="m-2 flex items-center border-b border-b-slate-300 text-lg">
+            Maturity: <Maturity value={score.maturity.value} />
+          </h2>
+          <ul className="w-full list-inside space-y-2 text-sm text-slate-500">
+            {score.maturity.notes.map((noteId, index) => (
+              <li key={index} className="mb-2 flex items-start">
+                <span className="h-5 px-2">
+                  <Icon path={mdiCircleSmall} size={0.5} />
+                </span>
+
+                {notes[noteId]?.note || `Unknown id ${noteId}`}
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
+      <div>
+        <h2 className="border-b border-b-slate-300">Package Stats</h2>
+        <PackageStats score={score} pkg={pkg} ecosystem={ecosystem} />
+        <h2 className="border-b border-b-slate-300">Source Stats</h2>
+        <OtherStats score={score} />
+      </div>
+    </div>
   );
 }
--- src/app/api/package/[ecosystem]/[...packageName]/route.ts
+++ src/app/api/package/[ecosystem]/[...packageName]/route.ts
@@ -1,5 +1,4 @@
-import { cachedNotes, Notes } from "@/utils/database";
-import packageScore from "@/utils/packageScore";
+import { fetchPackageScore } from "@/utils/score_res";
 import { NextRequest } from "next/server";
 
 type Props = {
@@ -9,37 +8,20 @@ type Props = {
   }>;
 };
 
-type T = {
-  health_risk?: { notes?: string[] };
-  maturity?: { notes?: string[] };
-};
-function replaceNoteIdWithTextCode(val: T, notes: Notes) {
-  if (val.health_risk?.notes != null) {
-    val.health_risk.notes = val.health_risk.notes.map(
-      (noteId) => notes[noteId].code || "Unknown",
-    );
-  }
-  if (val.maturity?.notes != null) {
-    val.maturity.notes = val.maturity.notes.map(
-      (noteId) => notes[noteId].code || "Unknown",
-    );
-  }
-}
-
 export async function GET(request: NextRequest, { params }: Props) {
   const { ecosystem, packageName } = await params;
   const name = packageName.join("/");
-  const { pkg, source } = await packageScore(ecosystem, name);
-  const notes = await cachedNotes();
+  const {
+    package: pkg,
+    status,
+    source,
+  } = await fetchPackageScore(ecosystem, name);
 
-  if (pkg == null || source == null) {
+  if (status === "not_found") {
     return Response.json({ error: "Not Found" }, { status: 404 });
   }
   // @ts-expect-error dont care about packages for json response
   delete source.packages;
-
-  replaceNoteIdWithTextCode(pkg, notes);
-  replaceNoteIdWithTextCode(source, notes);
 
   return Response.json({ ecosystem, name, source, pkg });
 }
