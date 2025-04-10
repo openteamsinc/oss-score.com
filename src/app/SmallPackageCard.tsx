9a900d35ea06abf799458c09007f64f3d3e3e544
Sean Ross-Ross <srossross@gmail.com>
2025-04-10 10:01:08 -0700

    fix (#82)

--- src/app/SmallPackageCard.tsx
+++ src/app/SmallPackageCard.tsx
@@ -11,10 +11,7 @@ export default function SmallPackageCard({ ecosystem, packageName }: Props) {
     <Link href={`/${ecosystem}/${packageName}`} className="block">
       <div className="flex items-center rounded-lg border border-gray-200 bg-white ">
         <div className="mr-3 flex size-12 shrink-0 items-center justify-center rounded-md bg-gray-50">
-          <EcosystemIcon
-            ecosystem={ecosystem}
-            className="size-6 text-gray-500"
-          />
+          <EcosystemIcon ecosystem={ecosystem} className="size-6" />
         </div>
         <div className="truncate font-medium" title={packageName}>
           {packageName}
